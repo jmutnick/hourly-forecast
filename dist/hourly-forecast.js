6adeeebc1eacb8b13aeb5cc4bc5d2ae9cac1bee4
Jonathan Mutnick <jmutnick@users.noreply.github.com>
2020-08-12 12:46:23 -0400

    Update hourly-forecast.js
    Simplified chart to bare minimum

--- dist/hourly-forecast.js
+++ dist/hourly-forecast.js
@@ -373,44 +373,20 @@ Graph below
 
   
 <script>
-	  var ctx = document.getElementById('myChart');
-//var 
+var ctx = document.getElementById('myChart');
+ 
 myChart = new Chart(ctx, {
   type: 'line',
   data: {
       labels: ["H0", "Hour1H", "Hour2H", "Hour3H"],
       datasets: [{
         label: 'Temperature F',
-        data: [1,2,3,4],
-        backgroundColor: [
-            'rgba(255, 99, 132, 0.2)',
-            'rgba(54, 162, 235, 0.2)',
-            'rgba(255, 206, 86, 0.2)',
-            'rgba(75, 192, 192, 0.2)',
-            'rgba(153, 102, 255, 0.2)',
-            'rgba(255, 159, 64, 0.2)'
-             ],
-        borderColor: [
-            'rgba(255, 99, 132, 1)',
-            'rgba(54, 162, 235, 1)',
-            'rgba(255, 206, 86, 1)',
-            'rgba(75, 192, 192, 1)',
-            'rgba(153, 102, 255, 1)',
-            'rgba(255, 159, 64, 1)'
-         ],
+        data: [1,2,3,4],       
         borderWidth: 1,
 	  fill: false
         }]
     },
-    options: {
-        scales: {
-            yAxes: [{
-                ticks: {
-                    beginAtZero: false
-                }
-            }]
-        }
-    }
+    
 });
 </script>
 `;
