1dc830514d679cbe790a364eca2fcf0d2c832b3a
jmutnick <jmutnick@umich.edu>
2020-08-31 12:30:44 -0400

    Fix variables

--- dist/hourly-forecast.js
+++ dist/hourly-forecast.js
@@ -19,7 +19,18 @@ class HourlyForecast extends HTMLElement {
     const sunsetdt = new Date(hass.states[this.config.sun_object].attributes.next_setting);
     
     // Variable Declarations
-    import './variables.js';
+    const uiDateOptions = { hour: 'numeric', };  
+	var Precip_state = new Array();
+	var Precipprob_state = new Array();
+	var Temp_state = new Array();
+	var Humid_state = new Array()
+	var Weather_state = new Array();
+	var FeelsLikeTemp_state = new Array();
+	var event = new Array();  
+	var Hour = new Array();
+	var numF=4;
+	var html1;
+	var Precip, PrecipProb, Temp, Humid, Weather, FeelsLike, HourNum;
     
     
     //  this is start of 0h
