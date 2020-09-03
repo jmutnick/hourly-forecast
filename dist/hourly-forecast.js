f0e2ef5757f479247721bf86cce61166d65468a7
jmutnick <jmutnick@umich.edu>
2020-09-03 11:23:36 -0400

    web version of chart.js

--- dist/hourly-forecast.js
+++ dist/hourly-forecast.js
@@ -177,7 +177,7 @@ html1 += `</table>`;
 if (graph == true) {
       html1 += `
       <head>
-	  	<script src="/local/community/hourly-forecast/helpers/Chart.js"></script>
+	  	<script src="https://unpkg.com/chart.js@2.9.3/dist/Chart.min.js?module"></script>
       </head>
       <div>
       	<canvas id="myChart" width="100%" height="250""></canvas>
