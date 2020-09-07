5a5ba6a386203e6d46445dcfc6a7782cb48e414d
jmutnick <jmutnick@umich.edu>
2020-09-07 09:52:43 -0400

    Closer

--- dist/hourly-forecast.js
+++ dist/hourly-forecast.js
@@ -176,7 +176,7 @@ tr.border_bottom  td{
       
 html1 += `</table><canvas id="MyCanvas" name"myChart" width="100%"></canvas>`;
 
-console.log("Result: " + this.content.document.getElementsByName('MyCanvas'));
+console.log("Result: " + this.content.document.getElementById('MyCanvas'));
 
 html1 += `</body>`;
 
