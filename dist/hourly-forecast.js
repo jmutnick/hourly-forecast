f8b0e3abcc26bd934006d2c069fbe9116e6ac87a
jmutnick <jmutnick@umich.edu>
2020-09-09 17:30:14 -0400

    Debugging

--- dist/hourly-forecast.js
+++ dist/hourly-forecast.js
@@ -145,12 +145,13 @@ tr.border_bottom  td{
             <div>${Precip} in/hr</div></td></tr>`;
       };
       
-html1 += '</table><canvas id="MyCanvas" width="100%"></canvas>';
-html1 += '<script language=javascript>
-var ctx = document.getElementById("MyCanvas");
-console.log("Result: " + ctx);
-</script>'
-html1 += `</body>`;
+html1 += `</table>
+<div><canvas id="MyCanvas" width="100%"></canvas></div>;
+<script language=javascript>
+	var ctx = document.getElementById("MyCanvas");
+	console.log("Result: " + ctx);
+</script>
+</body>`;
 
 this.content.innerHTML = html1;
 
